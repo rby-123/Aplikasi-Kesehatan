e5515ba4da40113cec4b11ed2f985d6be0abaefc
rby-123 <aisyah100809@gmail.com>
2025-03-25 07:13:05 +0700

    menambahkan timer

--- second_win.py
+++ second_win.py
@@ -1,13 +1,22 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # checking the types of input values
 from PyQt5.QtWidgets import (
        QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
        QGroupBox, QRadioButton,
        QPushButton, QLabel, QListWidget, QLineEdit)
 
 
 from instr import *
 from final_win import *
+
+
+class Experiment():
+   def __init__(self, age, test1, test2, test3):
+       self.age = age
+       self.t1 = test1
+       self.t2 = test2
+       self.t3 = test3
 
 
 class TestWin(QWidget):
@@ -29,14 +38,6 @@ class TestWin(QWidget):
       
        # start:
        self.show()
-  
-   def next_click(self):
-       self.tw = TestWin()
-       self.hide()
-
-
-   def connects(self):
-       self.btn_next.clicked.connect(self.next_click)
 
 
    ''' sets what the window will look like (label, size, location) '''
@@ -48,7 +49,6 @@ class TestWin(QWidget):
 
    def initUI(self):
        ''' creates graphic elements '''
-       #self.questionnary = AllQuestions()
        self.btn_next = QPushButton(txt_sendresults, self)
        self.btn_test1 = QPushButton(txt_starttest1, self)
        self.btn_test2 = QPushButton(txt_starttest2, self)
@@ -63,6 +63,7 @@ class TestWin(QWidget):
        self.text_test2 = QLabel(txt_test2)
        self.text_test3 = QLabel(txt_test3)
        self.text_timer = QLabel(txt_timer)
+       self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
 
 
        self.line_name = QLineEdit(txt_hintname)
@@ -101,18 +102,75 @@ class TestWin(QWidget):
        self.h_line.addLayout(self.l_line) 
        self.h_line.addLayout(self.r_line)       
        self.setLayout(self.h_line)
-  
+
+
    def next_click(self):
        self.hide()
-       self.fw = FinalWin()
+       self.exp = Experiment(int(self.line_age.text()), self.line_test1.text(), self.line_test2.text(), self.line_test3.text())
+       self.fw = FinalWin(self.exp)
+
+
+   def timer_test(self):
+       global time
+       time = QTime(0, 0, 15)
+       self.timer = QTimer()
+       self.timer.timeout.connect(self.timer1Event)
+       self.timer.start(1000)
+
+
+   def timer_sits(self):
+       global time
+       time = QTime(0, 0, 30)
+       self.timer = QTimer()
+       self.timer.timeout.connect(self.timer2Event)
+       self.timer.start(1500)
+
+
+   def timer_final(self):
+       global time
+       time = QTime(0, 1, 0)
+       self.timer = QTimer()
+       self.timer.timeout.connect(self.timer3Event)
+       self.timer.start(1000)
+
+
+   def timer1Event(self):
+       global time
+       time = time.addSecs(-1)
+       self.text_timer.setText(time.toString("hh:mm:ss"))
+       self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+       self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+       if time.toString("hh:mm:ss") == "00:00:00":
+           self.timer.stop()
+
+
+   def timer2Event(self):
+       global time
+       time = time.addSecs(-1)
+       self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
+       self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+       self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+       if time.toString("hh:mm:ss") == "00:00:00":
+           self.timer.stop()
+
+
+   def timer3Event(self):
+       global time
+       time = time.addSecs(-1)
+       self.text_timer.setText(time.toString("hh:mm:ss"))
+       if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+           self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+       elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+           self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+       else:
+           self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+       self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+       if time.toString("hh:mm:ss") == "00:00:00":
+           self.timer.stop()
 
 
    def connects(self):
        self.btn_next.clicked.connect(self.next_click)
-
-
-   ''' sets what the window will look like (label, size, location) '''
-   def set_appear(self):
-       self.setWindowTitle(txt_title)
-       self.resize(win_width, win_height)
-       self.move(win_x, win_y)
+       self.btn_test1.clicked.connect(self.timer_test)
+       self.btn_test2.clicked.connect(self.timer_sits)
+       self.btn_test3.clicked.connect(self.timer_final)
